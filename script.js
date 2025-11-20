9776d075ce212a02f198ecf05e3a03bda5319577
Avnisharma15 <sharma02422@gmail.com>
2025-11-20 13:27:17 +0530

    food delivery project files

---
+++ script.js
@@ -0,0 +1,191 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+    },
+});
+
+
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+const menuList = document.querySelector('.menu-list');
+const cartList = document.querySelector('.cart-list');
+const cartTotal = document.querySelector('.cart-total');
+const cartValue = document.querySelector('.cart-value');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburger = document.querySelector('.hamburger');
+const bars = hamburger.querySelector('i');
+
+cartIcon.addEventListener('click', () =>
+    cartTab.classList.add('cart-tab-active')
+);
+
+closeBtn.addEventListener('click', () =>
+    cartTab.classList.remove('cart-tab-active')
+);
+
+hamburger.addEventListener('click', () =>
+    mobileMenu.classList.toggle('mobile-menu-active')
+);
+
+hamburger.addEventListener('click', () =>
+    bars.classList.toggle('fa-xmark')
+);
+
+hamburger.addEventListener('click', () =>
+    bars.classList.toggle('fa-bars')
+);
+
+
+
+let productList = [];
+let cartProduct = [];
+
+const updateTotals = () => {
+    let totalPrice = 0;
+    let totalQuantity = 0;
+
+    document.querySelectorAll('.item').forEach(item => {
+
+        const quantity = parseInt(item.querySelector('.quantity-value').textContent);
+        const price = parseFloat(item.querySelector('.item-total').textContent.replace('$', ''))
+
+        totalPrice += price;
+        totalQuantity += quantity;
+    });
+
+    cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+    cartValue.textContent = totalQuantity;
+}
+
+const showCarts = () => {
+    productList.forEach(product => {
+        const orderCard = document.createElement('div');
+        orderCard.classList.add('order-card');
+
+        orderCard.innerHTML = `
+        <div class="card-image">
+            <img src="${product.image}" alt="${product.name}" class="cart-img">
+        </div>
+        <h4>${product.name}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a href="#" class="btn card-btn">Add to Cart</a>
+        `;
+
+        menuList.appendChild(orderCard);
+
+        const cardBtn = orderCard.querySelector('.card-btn');
+        cardBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            addToCart(product);
+        })
+    })
+}
+
+const addToCart = (product) => {
+
+    const existingProduct = cartProduct.find(item => item.id === product.id);
+    if (existingProduct) {
+        alert('Item already in your cart!');
+        return;
+    }
+    cartProduct.push(product);
+
+    let quantity = 1;
+    let price = parseFloat(product.price.replace('$', ''));
+
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('item');
+
+    cartItem.innerHTML = `
+        <div class="item-image">
+            <img src="${product.image}" alt="Burger">
+        </div>
+        <div class="detail">
+            <h4>${product.name}</h4>
+            <h4 class="item-total">${product.price}</h4>
+        </div>
+        <div style="display: flex; align-items: center;">
+            <a href="#" class="quantity-btn minus"><i class="fa-solid fa-minus"></i></a>
+            <h4 class="quantity-value">${quantity}</h4>
+            <a href="#" class="quantity-btn plus"><i class="fa-solid fa-plus"></i></a>
+        </div>  
+    `;
+
+    cartList.appendChild(cartItem);
+    updateTotals();
+
+    const plusBtn = cartItem.querySelector('.plus');
+    const quantityValue = cartItem.querySelector('.quantity-value');
+    const itemTotal = cartItem.querySelector('.item-total');
+
+    const minusBtn = cartItem.querySelector('.minus');
+
+    plusBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        quantity++;
+        quantityValue.textContent = quantity;
+        itemTotal.textContent = `$${(price * quantity).toFixed(2)}`
+        updateTotals();
+    });
+
+    minusBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (quantity > 1) {
+
+            quantity--;
+            quantityValue.textContent = quantity;
+            itemTotal.textContent = `$${(price * quantity).toFixed(2)}`
+            updateTotals();
+        }
+        else {
+            cartItem.classList.add('slide-out')
+
+            setTimeout(() => {
+                cartItem.remove();
+                cartProduct = cartProduct.filter(item => item.id !== product.id);
+                updateTotals();
+            }, 300)
+        }
+
+
+    });
+}
+
+const initApp = () => {
+    fetch('products.json').then
+        (response => response.json()).then
+        (data => {
+            productList = data;
+            showCarts();
+        });
+}
+initApp();
+
+// document.addEventListener("scroll", function () {
+//     const navbar = document.querySelector(".navbar");
+//     const workSection = document.querySelector("#work");
+
+//     if (window.scrollY >= workSection.offsetTop - 80) { 
+//         navbar.classList.add("scrolled");
+//     } else {
+//         navbar.classList.remove("scrolled");
+//     }
+// });
+
+document.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    const heroSection = document.querySelector("#hero"); 
+
+    if (window.scrollY > heroSection.offsetHeight - 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
